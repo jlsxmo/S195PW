6ce8ff226e89d69b3503e8052df420dcc251e721
Juan Luis Mosqueda Orta <122045310@upq.edu.mx>
2024-10-13 16:42:46 -0600

    Práctica 4
    Controladores

--- IntroLaravel/routes/web.php
+++ IntroLaravel/routes/web.php
@@ -1,12 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ControladorVistas;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 
+Route::get('/', [ControladorVistas::class, 'home'])->name('rutainicio');
+Route::get('/formulario', [ControladorVistas::class, 'formulario'])->name('rutaformulario');
+Route::get('/clientes', [ControladorVistas::class, 'consulta'])->name('rutaclientes');
+Route::get('/componentes', [ControladorVistas::class, 'componentes'])->name('rutaComponentes');
+
+
+/* Manejo antiguo de vistas
+Route::get('/', function () {
+     return view('welcome');
+});
+*/
+
+/* Manejo de vistas
 Route::view('/','inicio')->name('rutainicio');
 Route::view('/formulario','formulario')->name('rutaformulario');
 Route::view('/clientes', 'clientes')->name('rutaclientes');
 Route::view('/componentes', 'componentes')->name('rutaComponentes');
+ */
