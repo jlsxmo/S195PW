8380571a64234b47180d2536cd069a166bfd5bb6
Juan Luis Mosqueda Orta <122045310@upq.edu.mx>
2024-10-14 18:19:09 -0600

    Práctica 5
    Peticiones (Request)

--- IntroLaravel/app/Http/Controllers/controladorVistas.php
+++ IntroLaravel/app/Http/Controllers/controladorVistas.php
@@ -4,20 +4,35 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class controladorVistas extends Controller
+class ControladorVistas extends Controller
 {
     public function home()
     {
         return view('inicio');
     }
-
     public function formulario()
     {
         return view('formulario');
     }
-
     public function consulta()
     {
         return view('clientes');
     }
+    public function procesarCliente(Request $peticion)
+    {
+        //return 'Llego tu formulario al controlador';
+        
+        //Imprimimos todo lo que viene en la peticion
+        //return $peticion->all();
+
+        //Devuelve la ruta de la petición
+        //return $peticion->path();
+
+        //Imprime la url completa de la peticion
+        //return $peticion->url();
+
+        //Muestra la ip origen de la peticion
+        //return $peticion->ip();
+    }
+
 }
--- IntroLaravel/routes/web.php
+++ IntroLaravel/routes/web.php
@@ -3,22 +3,31 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ControladorVistas;
 
+Route::get('/',[ControladorVistas::class,'home'])->name('rutainicio');
+Route::get('/formulario',[ControladorVistas::class,'formulario'])->name('rutaformulario');
+Route::get('/clientes',[ControladorVistas::class,'consulta'])->name('rutaclientes');
+Route::get('/componentes',[ControladorVistas::class,'componentes'])->name('rutacomponentes');
 
-Route::get('/', [ControladorVistas::class, 'home'])->name('rutainicio');
-Route::get('/formulario', [ControladorVistas::class, 'formulario'])->name('rutaformulario');
-Route::get('/clientes', [ControladorVistas::class, 'consulta'])->name('rutaclientes');
-Route::get('/componentes', [ControladorVistas::class, 'componentes'])->name('rutaComponentes');
+Route::post('/enviar',[ControladorVistas::class,'procesarCliente'])->name('procesar');
 
 
-/* Manejo antiguo de vistas
-Route::get('/', function () {
-     return view('welcome');
+
+
+
+
+//MANEJO ANTIGUO DE VISTAS
+/*  Route::get('/', function () {
+    return view('componentes');
 });
-*/
-
-/* Manejo de vistas
-Route::view('/','inicio')->name('rutainicio');
-Route::view('/formulario','formulario')->name('rutaformulario');
-Route::view('/clientes', 'clientes')->name('rutaclientes');
-Route::view('/componentes', 'componentes')->name('rutaComponentes');
  */
+
+
+/*  shit + alt + a
+Route::view('/','inicio',)->name('rutainicio'); 
+
+Route::view('/formulario','formulario')->name('rutaformulario'); 
+
+Route::view('/clientes','clientes')->name('rutaclientes');
+
+Route::view('/componentes','componentes')->name('rutacomponentes');
+*/
