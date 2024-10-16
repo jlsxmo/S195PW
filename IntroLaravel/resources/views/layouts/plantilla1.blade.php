fe4c776995c3af861a39416684956571340d5aaa
Juan Luis Mosqueda Orta <122045310@upq.edu.mx>
2024-10-16 15:27:56 -0600

    Crear proyecto "practicaRepasos"

--- IntroLaravel/resources/views/layouts/plantilla1.blade.php
+++ IntroLaravel/resources/views/layouts/plantilla1.blade.php
@@ -18,10 +18,10 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link {{ request()->routeIs('rutaformulario')?'text-warnig':'' }}" aria-current="page" href="{{ route('rutaformulario') }}">Registro Clientes</a>
+                        <a class="nav-link {{ request()->routeIs('rutaformulario')?'text-primary':'' }}" aria-current="page" href="{{ route('rutaformulario') }}">Registro Clientes</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link {{ request()->routeIs('rutaclientes')?'text-primary':'' }}" href="{{ route('rutaclientes') }}">Clientes</a>
+                        <a class="nav-link {{ request()->routeIs('rutaclientes')?'text-warning':'' }}" href="{{ route('rutaclientes') }}">Consulta Clientes</a>
                     </li>
                 </ul>
             </div>
